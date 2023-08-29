fe75ce6efcfb4d8e93e504440212d74da22b5756
Aimen81 <60570709+Aimen81@users.noreply.github.com>
2023-08-29 13:10:34 +0200

    Create new Component

---
+++ src/app/aimen/aimen.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-aimen',
+  templateUrl: './aimen.component.html',
+  styleUrls: ['./aimen.component.sass']
+})
+export class AimenComponent {
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { RegisterComponent } from './register/register.component';
+import { AimenComponent } from './aimen/aimen.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    RegisterComponent
+    RegisterComponent,
+    AimenComponent
   ],
   imports: [
     BrowserModule,
