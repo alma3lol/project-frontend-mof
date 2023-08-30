167dbe8ff2d25e217dd485632a721ae890bdfdd1
Omar Almalol <omar.alma3lol@gmail.com>
2023-08-30 11:31:09 +0200

    adds routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,33 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AimenComponent } from './aimen/aimen.component';
+import { MohamedComponent } from './mohamed/mohamed.component';
+import { HasanComponent } from './hasan/hasan.component';
+import { AlasekriComponent } from './alasekri/alasekri.component';
+import { RegisterComponent } from './register/register.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'aimen',
+    component: AimenComponent
+  },
+  {
+    path: 'malek',
+    component: AlasekriComponent
+  },
+  {
+    path: 'hasan',
+    component: HasanComponent
+  },
+  {
+    path: 'mohammed',
+    component: MohamedComponent
+  },
+  {
+    path: 'register',
+    component: RegisterComponent
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
