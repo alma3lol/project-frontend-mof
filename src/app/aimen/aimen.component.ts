1dcc20bdc3eadb7d0af633e23af178ef923ebbab
Omar Almalol <omar.alma3lol@gmail.com>
2023-08-30 13:01:57 +0200

    fix(aimen): fixes style file name

--- src/app/aimen/aimen.component.ts
+++ src/app/aimen/aimen.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-aimen',
   templateUrl: './aimen.component.html',
-  styleUrls: ['./aimen.component.sass']
+  styleUrls: ['./aimen.component.scss']
 })
 export class AimenComponent {
 
