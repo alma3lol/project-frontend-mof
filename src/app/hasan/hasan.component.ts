773b099d3d22241f5bacbb5b2d305c50f3e33b39
hasan esuri <141579712+hassan7esuri@users.noreply.github.com>
2023-08-30 03:41:04 -0700

    change from sacss to scss

--- src/app/hasan/hasan.component.ts
+++ src/app/hasan/hasan.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-hasan',
   templateUrl: './hasan.component.html',
-  styleUrls: ['./hasan.component.sass']
+  styleUrls: ['./hasan.component.scss']
 })
 export class HasanComponent {
 
